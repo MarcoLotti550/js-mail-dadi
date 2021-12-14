f39fd160457ad9cb1dff88dab18f4de461646cc3
MarcoLotti <gorlandelli64@gmail.com>
2021-12-14 10:31:23 +0100

    conteggio punteggi dei dadi

--- dadi/script.js
+++ dadi/script.js
@@ -1,17 +1,24 @@
 const risultato = document.getElementById("risultato");//const dei risultati
 
 let dice = document.querySelectorAll("img");//tutti i tag img ora vengono visti come dice
+let playerPoints = (0);
+let cpuPoints = (0);
+document.getElementById("playerPoints").innerHTML = playerPoints;
+document.getElementById("cpuPoints").innerHTML = cpuPoints;
 
 function gioca() {
     let dice1 = Math.floor(Math.random(0)*6 + 1);
     let dice2 = Math.floor(Math.random(0)*6 + 1);
-    let playerWin = document.getElementById(playerPoints);
-    let cpuWin = document.getElementById(cpuPoints);
     
     if (dice1 > dice2){
         risultato.innerHTML = '<div class="risultati">' + "HAI " + "VINTO" +'</div>';
+        playerPoints = (playerPoints + 1);
+        document.getElementById("playerPoints").innerHTML = playerPoints;
+
     }else if (dice1 < dice2){
         risultato.innerHTML = '<div class="risultati">' + "HAI " + "PERSO" +'</div>';
+        cpuPoints = (cpuPoints + 1);
+        document.getElementById("cpuPoints").innerHTML = cpuPoints;
 
     }else if (dice1 == dice2){
         risultato.innerHTML = '<div class="risultati">' + "PAREGGIO" +'</div>';
